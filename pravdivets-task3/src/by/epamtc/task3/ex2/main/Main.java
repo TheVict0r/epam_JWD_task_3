0f225f1f77a8900191d62bfe6e9f9899d5adbee0
vpravd1@gmail.com <Vv77100369>
2021-04-12 08:52:23 +0300

    1

--- pravdivets-task3/src/by/epamtc/task3/ex2/main/Main.java
+++ pravdivets-task3/src/by/epamtc/task3/ex2/main/Main.java
@@ -3,7 +3,12 @@ package by.epamtc.task3.ex2.main;
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		int[][] jagArr = new int[5][];
+		jagArr[0] = new int[]{5,  8,  9,   42};
+		jagArr[1] = new int[]{25, 1,  348};
+		jagArr[2] = new int[]{99, 7,  52,  42, 73, 72};
+		jagArr[3] = new int[]{24, 37, 569, 42, 99};
+		jagArr[4] = new int[]{29, 48, 18,  26, 81, 54, 3};
 
 	}
 
