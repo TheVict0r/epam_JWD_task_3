6c64d2717fac0b98be1f920334da3f9b62a1332e
vpravd1@gmail.com <Vv77100369>
2021-04-12 08:53:31 +0300

    1

--- pravdivets-task3/src/by/epamtc/task3/ex2/main/Main.java
+++ pravdivets-task3/src/by/epamtc/task3/ex2/main/Main.java
@@ -8,7 +8,7 @@ public class Main {
 		jagArr[1] = new int[]{25, 1,  348};
 		jagArr[2] = new int[]{99, 7,  52,  42, 73, 72};
 		jagArr[3] = new int[]{24, 37, 569, 42, 99};
-		jagArr[4] = new int[]{29, 48, 18,  26, 81, 54, 3};
+		jagArr[4] = new int[]{29, 48, 18,  24, 81, 54, 3};
 
 	}
 
